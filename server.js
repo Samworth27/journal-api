842b88608835f1ec793602bd1c04d6baf0f29b6d
Sam <samworth27@gmail.com>
2022-06-28 10:46:48 +1000

    + get & post for entries + uuid gen

--- server.js
+++ server.js
@@ -2,12 +2,47 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+function uuid() {
+  let uuid = "";
+  for (let i = 0; i < 16; i++) {
+    let digit = Math.floor(Math.random() * 256);
+    if (i == 4 || i == 6 || i == 8) {
+      uuid += "-";
+    }
+    uuid += digit.toString(16).padStart(2, "0");
+  }
+  return uuid;
+}
+
 const categories = ["food", "health", "tech", "software"];
+
+const entries = [
+  {id: uuid(), category: "food", entry: "yum i liek food"},
+  {id: uuid(), category: "health", entry: "monster and a durry = helth"},
+  {id: uuid(), category: "tech", entry: "computer go beep boop"},
+  {id: uuid(), category: "software", entry: "my type words for my computer go beep boop"},
+]
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send({ name: "Journal API", endpoints: ["/categories"] });
+  res.send({ name: "Journal API", endpoints: ["/categories","/entries"] });
 });
+
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/entries", (req, res) => {
+  res.send(entries);
+});
+
+app.post("/entries", (req, res)=>{
+  const entry = {id:uuid()}
+  entry.category = req.body.category;
+  entry.entry = req.body.entry
+  entries.push(entry);
+  res.send(entry);
 });
 
 app.listen(port, () => {
