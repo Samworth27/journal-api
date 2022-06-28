20bed23748a590fc8d040831523b38df92905e38
Sam <samworth27@gmail.com>
2022-06-28 14:34:49 +1000

    + exported mongoose init

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+// setup mongoose
+const mongoose = require("mongoose");
+// const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.cdcgg.mongodb.net/?retryWrites=true&w=majority`;
+const MONGODB_URI = `mongodb+srv://xxvii:${process.env.MONGODB_PASS}@cluster0.cdcgg.mongodb.net/journal?retryWrites=true&w=majority`;
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    let state = mongoose.connection.states[mongoose.connection.readyState];
+    console.log(`Mongoose: ${state}`);
+  })
+  .catch((error) => console.log(error));
+
+module.exports = { mongoose };
