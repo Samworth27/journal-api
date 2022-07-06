7ec249b4abd8ce97f2192ddb21df6d7bf87dd688
Sam Mitchell <samworth27@gmail.com>
2022-07-06 10:57:27 +1000

    + category api

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 // get routes
 const entryRoutes = require("./entry_routes.js");
+const categoryRoutes = require("./category_routes.js");
+
 
 router.get("/", function(req, res, next) {
   console.log("hit")
@@ -18,10 +20,7 @@ router.get("/endpoints", (req, res) => {
   // res.send([Object.keys(req), req.baseUrl])
 })
 
-router.get("/categories", (req, res) => {
-  res.send(categories);
-});
-
 router.use("/entries", entryRoutes);
+router.use("/categories", categoryRoutes);
 
 module.exports = router;
